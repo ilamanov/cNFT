683ab50d72f10ea76c41fc5cb2871725f3ea3c6e
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2022-06-28 17:43:14 +0200

    organize CryptoCoven

--- src/examples/CryptoCoven.sol
+++ src/examples/CryptoCoven.sol
@@ -4,6 +4,7 @@
 pragma solidity ^0.8.4;
 
 import "../interfaces/IERC721.sol";
+import "../interfaces/IERC165.sol";
 import "../interfaces/IERC2981.sol";
 import "../interfaces/IERC20.sol";
 
@@ -13,46 +14,18 @@ import "../extendable/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract CryptoCoven is IERC721, IERC2981, Ownable, ReentrancyGuard {
+contract CryptoCoven is IERC721, IERC165, IERC2981, Ownable, ReentrancyGuard {
     using Strings for uint256;
 
+    // ============ MAX LIMITS ============    
+    
     uint256 public constant MAX_WITCHES_PER_WALLET = 3;
     uint256 public constant PUBLIC_SALE_PRICE = 0.07 ether;
     uint256 public constant COMMUNITY_SALE_PRICE = 0.05 ether;
     uint256 public immutable maxWitches;
     uint256 public immutable maxGiftedWitches;
     uint256 public immutable maxCommunitySaleWitches;
-
-    uint256 private tokenCounter;
-    uint256 public numGiftedWitches;
-
-    string private baseURI;
-    string public verificationHash;
-    address private openSeaProxyRegistryAddress;
-    bool private isOpenSeaProxyActive = true;
-
-    bytes32 public communitySaleMerkleRoot;
-    bytes32 public claimListMerkleRoot;
-
-    bool public isPublicSaleActive;
-
-    bool public isCommunitySaleActive;
-
-    mapping(address => uint256) public communityMintCounts;
-    mapping(address => bool) public claimed;
-
-    // ============ ACCESS CONTROL/SANITY MODIFIERS ============
-
-    modifier publicSaleActive() {
-        require(isPublicSaleActive, "Public sale is not open");
-        _;
-    }
-
-    modifier communitySaleActive() {
-        require(isCommunitySaleActive, "Community sale is not open");
-        _;
-    }
-
+    
     modifier maxWitchesPerWallet(uint256 numberOfTokens) {
         require(
             balanceOf(msg.sender) + numberOfTokens <= MAX_WITCHES_PER_WALLET,
@@ -80,7 +53,7 @@ contract CryptoCoven is IERC721, IERC2981, Ownable, ReentrancyGuard {
         );
         _;
     }
-
+    
     modifier isCorrectPayment(uint256 price, uint256 numberOfTokens) {
         require(
             price * numberOfTokens == msg.value,
@@ -88,19 +61,7 @@ contract CryptoCoven is IERC721, IERC2981, Ownable, ReentrancyGuard {
         );
         _;
     }
-
-    modifier isValidMerkleProof(bytes32[] calldata merkleProof, bytes32 root) {
-        require(
-            MerkleProof.verify(
-                merkleProof,
-                root,
-                keccak256(abi.encodePacked(msg.sender))
-            ),
-            "Address does not exist in list"
-        );
-        _;
-    }
-
+    
     constructor(
         address _openSeaProxyRegistryAddress,
         uint256 _maxWitches,
@@ -113,7 +74,169 @@ contract CryptoCoven is IERC721, IERC2981, Ownable, ReentrancyGuard {
         maxGiftedWitches = _maxGiftedWitches;
     }
 
-    // ============ PUBLIC FUNCTIONS FOR MINTING ============
+    // ============ SALE STATE ============
+
+    bool public isPublicSaleActive;
+    bool public isCommunitySaleActive;
+    
+    function setIsPublicSaleActive(bool _isPublicSaleActive)
+        external
+        onlyOwner
+    {
+        isPublicSaleActive = _isPublicSaleActive;
+    }
+
+    function setIsCommunitySaleActive(bool _isCommunitySaleActive)
+        external
+        onlyOwner
+    {
+        isCommunitySaleActive = _isCommunitySaleActive;
+    }
+    
+    modifier publicSaleActive() {
+        require(isPublicSaleActive, "Public sale is not open");
+        _;
+    }
+
+    modifier communitySaleActive() {
+        require(isCommunitySaleActive, "Community sale is not open");
+        _;
+    }
+    
+    // ============ ALLOWLISTS ============
+
+    bytes32 public communitySaleMerkleRoot;
+    bytes32 public claimListMerkleRoot;
+    
+    function setCommunityListMerkleRoot(bytes32 merkleRoot) external onlyOwner {
+        communitySaleMerkleRoot = merkleRoot;
+    }
+
+    function setClaimListMerkleRoot(bytes32 merkleRoot) external onlyOwner {
+        claimListMerkleRoot = merkleRoot;
+    }
+    
+    modifier isValidMerkleProof(bytes32[] calldata merkleProof, bytes32 root) {
+        require(
+            MerkleProof.verify(
+                merkleProof,
+                root,
+                keccak256(abi.encodePacked(msg.sender))
+            ),
+            "Address does not exist in list"
+        );
+        _;
+    }
+    
+    // ============ TRACKERS ============
+
+    uint256 private tokenCounter;
+    uint256 public numGiftedWitches;
+    
+    function getLastTokenId() external view returns (uint256) {
+        return tokenCounter;
+    }
+    
+    function nextTokenId() private returns (uint256) {
+        unchecked {
+            tokenCounter++;
+        }
+        return tokenCounter;
+    }
+    
+    // ============ GASLESS OS LISTING ============
+    
+    address private immutable openSeaProxyRegistryAddress;
+    bool private isOpenSeaProxyActive = true;
+    
+    // function to disable gasless listings for security in case
+    // opensea ever shuts down or is compromised
+    function setIsOpenSeaProxyActive(bool _isOpenSeaProxyActive)
+        external
+        onlyOwner
+    {
+        isOpenSeaProxyActive = _isOpenSeaProxyActive;
+    }
+    
+    /**
+     * @dev Override isApprovedForAll to allowlist user's OpenSea proxy accounts to enable gas-less listings.
+     */
+    function isApprovedForAll(address owner, address operator)
+        public
+        view
+        override
+        returns (bool)
+    {
+        // Get a reference to OpenSea's proxy registry contract by instantiating
+        // the contract using the already existing address.
+        ProxyRegistry proxyRegistry = ProxyRegistry(
+            openSeaProxyRegistryAddress
+        );
+        if (
+            isOpenSeaProxyActive &&
+            address(proxyRegistry.proxies(owner)) == operator
+        ) {
+            return true;
+        }
+
+        return super.isApprovedForAll(owner, operator);
+    }
+    
+    // ============ TOKEN URI ============
+    
+    string private baseURI;
+    
+    function getBaseURI() external view returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string memory _baseURI) external onlyOwner {
+        baseURI = _baseURI;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(_exists(tokenId), "Nonexistent token");
+
+        return
+            string(abi.encodePacked(baseURI, "/", tokenId.toString(), ".json"));
+    }
+    
+    // ============ VERIFICATION HASH ============
+    
+    string public verificationHash;
+    
+    function setVerificationHash(string memory _verificationHash)
+        external
+        onlyOwner
+    {
+        verificationHash = _verificationHash;
+    }
+    
+    // ============ WITHDRAWALS ============
+    
+    function withdraw() public onlyOwner {
+        uint256 balance = address(this).balance;
+        payable(msg.sender).transfer(balance);
+    }
+
+    function withdrawTokens(IERC20 token) public onlyOwner {
+        uint256 balance = token.balanceOf(address(this));
+        token.transfer(msg.sender, balance);
+    }
+    
+    // ============ MINTING ============
+    
+    mapping(address => uint256) public communityMintCounts;
+    mapping(address => bool) public claimed;
 
     function mint(uint256 numberOfTokens)
         external
@@ -174,59 +297,7 @@ contract CryptoCoven is IERC721, IERC2981, Ownable, ReentrancyGuard {
         _safeMint(msg.sender, nextTokenId());
     }
 
-    // ============ PUBLIC READ-ONLY FUNCTIONS ============
-
-    function getBaseURI() external view returns (string memory) {
-        return baseURI;
-    }
-
-    function getLastTokenId() external view returns (uint256) {
-        return tokenCounter;
-    }
-
-    // ============ OWNER-ONLY ADMIN FUNCTIONS ============
-
-    function setBaseURI(string memory _baseURI) external onlyOwner {
-        baseURI = _baseURI;
-    }
-
-    // function to disable gasless listings for security in case
-    // opensea ever shuts down or is compromised
-    function setIsOpenSeaProxyActive(bool _isOpenSeaProxyActive)
-        external
-        onlyOwner
-    {
-        isOpenSeaProxyActive = _isOpenSeaProxyActive;
-    }
-
-    function setVerificationHash(string memory _verificationHash)
-        external
-        onlyOwner
-    {
-        verificationHash = _verificationHash;
-    }
-
-    function setIsPublicSaleActive(bool _isPublicSaleActive)
-        external
-        onlyOwner
-    {
-        isPublicSaleActive = _isPublicSaleActive;
-    }
-
-    function setIsCommunitySaleActive(bool _isCommunitySaleActive)
-        external
-        onlyOwner
-    {
-        isCommunitySaleActive = _isCommunitySaleActive;
-    }
-
-    function setCommunityListMerkleRoot(bytes32 merkleRoot) external onlyOwner {
-        communitySaleMerkleRoot = merkleRoot;
-    }
-
-    function setClaimListMerkleRoot(bytes32 merkleRoot) external onlyOwner {
-        claimListMerkleRoot = merkleRoot;
-    }
+    // ============ GIFTING ============
 
     function reserveForGifting(uint256 numToReserve)
         external
@@ -255,16 +326,6 @@ contract CryptoCoven is IERC721, IERC2981, Ownable, ReentrancyGuard {
         }
     }
 
-    function withdraw() public onlyOwner {
-        uint256 balance = address(this).balance;
-        payable(msg.sender).transfer(balance);
-    }
-
-    function withdrawTokens(IERC20 token) public onlyOwner {
-        uint256 balance = token.balanceOf(address(this));
-        token.transfer(msg.sender, balance);
-    }
-
     function rollOverWitches(address[] calldata addresses)
         external
         nonReentrant
@@ -283,16 +344,7 @@ contract CryptoCoven is IERC721, IERC2981, Ownable, ReentrancyGuard {
         }
     }
 
-    // ============ SUPPORTING FUNCTIONS ============
-
-    function nextTokenId() private returns (uint256) {
-        unchecked {
-            tokenCounter++;
-        }
-        return tokenCounter;
-    }
-
-    // ============ FUNCTION OVERRIDES ============
+    // ============ ERC165 and ERC2981 ============
 
     function supportsInterface(bytes4 interfaceId)
         public
@@ -305,46 +357,7 @@ contract CryptoCoven is IERC721, IERC2981, Ownable, ReentrancyGuard {
             interfaceId == type(IERC2981).interfaceId ||
             super.supportsInterface(interfaceId);
     }
-
-    /**
-     * @dev Override isApprovedForAll to allowlist user's OpenSea proxy accounts to enable gas-less listings.
-     */
-    function isApprovedForAll(address owner, address operator)
-        public
-        view
-        override
-        returns (bool)
-    {
-        // Get a reference to OpenSea's proxy registry contract by instantiating
-        // the contract using the already existing address.
-        ProxyRegistry proxyRegistry = ProxyRegistry(
-            openSeaProxyRegistryAddress
-        );
-        if (
-            isOpenSeaProxyActive &&
-            address(proxyRegistry.proxies(owner)) == operator
-        ) {
-            return true;
-        }
-
-        return super.isApprovedForAll(owner, operator);
-    }
-
-    /**
-     * @dev See {IERC721Metadata-tokenURI}.
-     */
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
-    {
-        require(_exists(tokenId), "Nonexistent token");
-
-        return
-            string(abi.encodePacked(baseURI, "/", tokenId.toString(), ".json"));
-    }
+    
 
     /**
      * @dev See {IERC165-royaltyInfo}.
