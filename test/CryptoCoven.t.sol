534d0f6a40740dcfd2070c3c42867f39a09953c6
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2022-07-21 20:23:09 +0200

    add tests for mint and transfer

--- test/CryptoCoven.t.sol
+++ test/CryptoCoven.t.sol
@@ -2,14 +2,47 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/interfaces/IERC721C.sol";
 import "../src/examples/CryptoCoven/CryptoCoven.sol";
 
-contract CryptoCovenTest is Test {
-    function setUp() public {}
+contract User is IERC721Receiver {
+    CryptoCoven private immutable cryptoCoven;
 
-    function testCryptoCovenDeploy(address openSeaProxyRegistryAddress) public {
-        new CryptoCoven(
-            openSeaProxyRegistryAddress,
+    constructor(CryptoCoven _cryptoCoven) {
+        cryptoCoven = _cryptoCoven;
+    }
+
+    function mint(uint256 numberOfTokens) public payable {
+        cryptoCoven.mint{value: msg.value}(numberOfTokens);
+    }
+
+    function safeTransferFrom(address to) public {
+        cryptoCoven.safeTransferFrom(
+            address(this),
+            to,
+            /* tokenId= */
+            1
+        );
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure override returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+}
+
+contract CryptoCovenTest is Test {
+    CryptoCoven private cryptoCoven;
+    User private user;
+
+    function setUp() public {
+        cryptoCoven = new CryptoCoven(
+            /* openSeaProxyRegistryAddress= */
+            0x5180db8F5c931aaE63c74266b211F580155ecac8,
             /* maxWitches= */
             9999,
             /* maxCommunitySaleWitches= */
@@ -17,5 +50,31 @@ contract CryptoCovenTest is Test {
             /* maxGiftedWitches= */
             250
         );
+        cryptoCoven.setIsPublicSaleActive(true);
+        user = new User(cryptoCoven);
+        // mint one token that will be used for transfer
+        user.mint{value: 0.07 ether}(1);
+    }
+
+    function testDeploy(address _openSeaProxyRegistryAddress) public {
+        new CryptoCoven(
+            _openSeaProxyRegistryAddress,
+            /* maxWitches= */
+            9999,
+            /* maxCommunitySaleWitches= */
+            3333,
+            /* maxGiftedWitches= */
+            250
+        );
+    }
+
+    function testMint() public {
+        uint256 numberOfTokens = 2;
+        user.mint{value: numberOfTokens * 0.07 ether}(numberOfTokens);
+    }
+
+    function testTransfer(address to) public {
+        vm.assume(to > address(0));
+        user.safeTransferFrom(to);
     }
 }
--- test/CryptoCovenC.t.sol
+++ test/CryptoCovenC.t.sol
@@ -2,19 +2,52 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/examples/CryptoCovenC/CryptoCovenC.sol";
+import "../src/ERC721C.sol";
 import "../src/interfaces/IERC721C.sol";
+import "../src/interfaces/IERC721Receiver.sol";
+import "../src/examples/CryptoCovenC/CryptoCovenC.sol";
+
+contract User is IERC721Receiver {
+    CryptoCovenC private immutable cryptoCovenC;
+
+    constructor(CryptoCovenC _cryptoCovenC) {
+        cryptoCovenC = _cryptoCovenC;
+    }
+
+    function mint(uint256 numberOfTokens) public payable {
+        cryptoCovenC.mint{value: msg.value}(numberOfTokens);
+    }
+
+    function safeTransferFrom(address to) public {
+        cryptoCovenC.safeTransferFrom(
+            address(this),
+            to,
+            /* tokenId= */
+            1
+        );
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure override returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+}
 
 contract CryptoCovenCTest is Test {
-    function setUp() public {}
+    ERC721C private composableERC721;
+    CryptoCovenC private cryptoCovenC;
+    User private user;
 
-    function testCryptoCovenCDeploy(
-        address composableERC721,
-        address openSeaProxyRegistryAddress
-    ) public {
-        new CryptoCovenC(
+    function setUp() public {
+        composableERC721 = new ERC721C();
+        cryptoCovenC = new CryptoCovenC(
             IERC721C(composableERC721),
-            openSeaProxyRegistryAddress,
+            /* openSeaProxyRegistryAddress= */
+            0x5180db8F5c931aaE63c74266b211F580155ecac8,
             /* maxWitches= */
             9999,
             /* maxCommunitySaleWitches= */
@@ -22,5 +55,35 @@ contract CryptoCovenCTest is Test {
             /* maxGiftedWitches= */
             250
         );
+        cryptoCovenC.setIsPublicSaleActive(true);
+        user = new User(cryptoCovenC);
+        // mint one token that will be used for transfer
+        user.mint{value: 0.07 ether}(1);
+    }
+
+    function testDeploy(
+        address _composableERC721,
+        address _openSeaProxyRegistryAddress
+    ) public {
+        new CryptoCovenC(
+            IERC721C(_composableERC721),
+            _openSeaProxyRegistryAddress,
+            /* maxWitches= */
+            9999,
+            /* maxCommunitySaleWitches= */
+            3333,
+            /* maxGiftedWitches= */
+            250
+        );
+    }
+
+    function testMint() public {
+        uint256 numberOfTokens = 2;
+        user.mint{value: numberOfTokens * 0.07 ether}(numberOfTokens);
+    }
+
+    function testTransfer(address to) public {
+        vm.assume(to > address(0));
+        user.safeTransferFrom(to);
     }
 }
